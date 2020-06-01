8896ca3409ff6783f2b8bead3cfb73264c62db81
yezheng@Ubuntu <yezhengli9@outlook.com>
2020-06-01 14:45:50 -0400

    using-c-functions

---
+++ capi/common.h
@@ -0,0 +1,13 @@
+/* common.h */
+
+I isRemoteErr(K x) {
+    if(!x) {
+        fprintf(stderr, "Network error: %s\n", strerror(errno));
+        return 1;
+    } else if(-128 == xt) {
+        fprintf(stderr, "Error message returned : %s\\n", x-\>s); 
+        r0(x);
+        return 1;
+    }
+    return 0;
+}
