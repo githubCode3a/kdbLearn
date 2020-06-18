c44711bd28b64f7384b937868531ec6d91804b69
yezheng@Ubuntu <yezhengli9@outlook.com>
2020-06-17 23:27:16 -0400

    till WDC

--- UniBit/UniBitapi.h
+++ UniBit/UniBitapi.h
@@ -43,7 +43,7 @@ class UniBitapi{
 		      .append_query(U("startDate"), U("2020-01-01")) 
 		      .append_query(U("endDate"), U("2020-01-31")) 
 		      .append_query(U("dataType"), U("json")) 
-		      .append_query(U("accessKey"), U("H9cnnJ1DEaSaRgRiWaYjkASGoq7X9OcA"))
+		      .append_query(U("accessKey"), U("hv0ZoLZSj2ydTLTYoGVHFhpYxJtF1EC0"))
 		      // RsjvEpx--EWhP77HxToylcMkCPxtF6UA	yezhengli9@outlook.com
 		      // 1uQEaW_HL4hT4eDiAOUv9U-1D4dGnKjA	yezhenglimr9@gmail.com
 		      // JApS-XgN6FCqejbOq9X3dSBFiHj83-FM	yezheng@sas.upenn.edu
@@ -59,6 +59,7 @@ class UniBitapi{
 		      // x_sIM5r_u20hDiUut_5rwSCmAH9ICDDM	yezhengsde@gmail.com
 		      // 616o-p28DhUBCfSXALHyTCblvGNX3wuQ	yezhenglisde@gmail.com
 		      //H9cnnJ1DEaSaRgRiWaYjkASGoq7X9OcA	b111110046@live.cn
+		      //hv0ZoLZSj2ydTLTYoGVHFhpYxJtF1EC0 yezhengli9@icloud.com
 		      .to_string();
 		    std::cout<<"message\t" <<mess<< '\n';
 		    return client.request(methods::GET, mess);
